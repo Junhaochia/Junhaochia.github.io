12ab7134e167e220bc7bceb8f913e879400be9b5
Junhaochia <newboy.chia5@gmail.com>
2024-04-20 10:56:50 +0800

    add owadio

---
+++ assets/js/serviceworker/ord.js
@@ -0,0 +1,37 @@
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Caching
+const cacheName = "Junhao.page/ord";
+const precachedResources = ["/projects/ord"];
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+// On Service Worker install, preCache resources from precachedResources Array
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+async function networkFirst(request) {
+  const cache = await caches.open(cacheName);
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await cache.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+// When there is a fetch request, try to fetch Page if not load from Cache.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(networkFirst(event.request);
+});
+
+// Purge Cache when there is new Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.delete(cacheName));
+});
---
+++ collections/_projects/ord.md
@@ -0,0 +1,11 @@
+---
+title: 'ORD Countdown'
+description: 'Owadio!'
+layout: projects/ord
+date: 2024-04-20 10:49:24
+---
+
+# Owadio
+
+dd
+Hello
